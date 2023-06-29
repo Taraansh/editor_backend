926b48ac1c218e6622da1aa889df942b9744d137
Taraansh <taraanshnandwani123@gmail.com>
2023-06-29 20:56:22 +0530

    ..

--- editor_backend/settings.py
+++ editor_backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l!6x0bt6+s7!obq2+(i&8hh!^nzsx1y#o1cms!kmgb1vcc7qgx'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,11 +84,11 @@ WSGI_APPLICATION = 'editor_backend.wsgi.application'
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'editor_accounts',
-       'USER': 'postgres',
-       'PASSWORD': 'taraansh.1109',
-       'HOST': 'localhost',
-       'PORT': '5432',
+       'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
    }
 }
 
