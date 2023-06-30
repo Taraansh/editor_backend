1b6bef841bf26c29a9d2a8bcde329cdc567a0672
Taraansh <taraanshnandwani123@gmail.com>
2023-06-30 16:55:23 +0530

    Password change url added

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('tokensroutes/', views.get_routes, name='token_routes'),
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Paths to manage password change and reset
+    path('passchange/<str:email>/', views.password_change, name='change_password')
 ]
